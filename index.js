4bfb1417a6d9044d8d74c2a31865c93280e39195
michaelmira018-ui <michaelmira018@gmail.com>
2026-05-08 23:40:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function showMessage(){
-
-alert("Welcome To Global Job Agency");
-
+function searchJobs() {
+  alert("Searching jobs... (demo function)");
 }
+
+// Simple subscribe action
+document.querySelector(".newsletter button").addEventListener("click", function () {
+  let email = document.querySelector(".newsletter input").value;
+
+  if (email) {
+    alert("Subscribed successfully: " + email);
+  } else {
+    alert("Please enter an email.");
+  }
+});
