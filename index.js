aace3456624b0f73afb4d3848af2940bc32dd1fb
tpao66 <michaelmira018@gmail.com>
2026-05-10 07:52:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 function searchJobs() {
-  alert("Searching jobs... (demo function)");
-}
-
-// Simple subscribe action
-document.querySelector(".newsletter button").addEventListener("click", function () {
-  let email = document.querySelector(".newsletter input").value;
-
-  if (email) {
-    alert("Subscribed successfully: " + email);
-  } else {
-    alert("Please enter an email.");
-  }
-});
-
-function searchJobs() {
   alert("Searching available jobs...");
 }
 
-/* SIDEBAR MENU */
+/* TOGGLE MENU */
 function toggleMenu() {
+
   const nav = document.getElementById("navMenu");
+
   nav.classList.toggle("active");
+
+  /* ADD HISTORY FOR BACK BUTTON */
+  if(nav.classList.contains("active")){
+    history.pushState({menu:true}, "");
+  }
 }
+
+/* CLOSE MENU WITH PHONE BACK BUTTON */
+window.onpopstate = function () {
+
+  const nav = document.getElementById("navMenu");
+
+  if(nav.classList.contains("active")){
+    nav.classList.remove("active");
+  }
+};
+
+/* CLOSE MENU WHEN SCREEN IS TAPPED */
+document.addEventListener("click", function(event){
+
+  const nav = document.getElementById("navMenu");
+
+  const menuIcon = document.querySelector(".menu-icon");
+
+  if(
+    !nav.contains(event.target) &&
+    !menuIcon.contains(event.target)
+  ){
+    nav.classList.remove("active");
+  }
+
+});
